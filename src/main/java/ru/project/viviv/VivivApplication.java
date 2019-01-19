7e796ad389a070ac15fe0c3344b26942849d9691
Aleksei Artemev <art.bmstu@gmail.com>
2019-01-19 14:47:09 +0300

    Initial commit

---
+++ src/main/java/ru/project/viviv/VivivApplication.java
@@ -0,0 +1,13 @@
+package ru.project.viviv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivivApplication.class, args);
+    }
+
+}
