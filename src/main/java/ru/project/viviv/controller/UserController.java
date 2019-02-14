c9850f67d9d77f95ad72802b34787ef126f9e8de
artemev <a.artemev@ntc-vulkan.ru>
2019-02-14 19:12:16 +0300

    [+] доступ к странице пользователя для ее владельца

--- src/main/java/ru/project/viviv/controller/UserController.java
+++ src/main/java/ru/project/viviv/controller/UserController.java
@@ -1,15 +1,28 @@
 package ru.project.viviv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import ru.project.viviv.model.entity.User;
+import ru.project.viviv.model.service.UserService;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
-    @RequestMapping("/")
-    public String user() {
-        return "user";
+    @GetMapping("{username}")
+    public ModelAndView user(@PathVariable(name = "username") String username, Principal principal) {
+        User user = userService.findByEmail(principal.getName());
+        if (user.getUsername().equals(username))
+            return new ModelAndView("profile", "user", user);
+        return new ModelAndView("/");
     }
 
     @RequestMapping("/profile")
