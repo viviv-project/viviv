34af61567c7c47149c3f00714cad16e162f48577
Aleksei Artemev <art.bmstu@gmail.com>
2019-02-24 22:45:28 +0300

    [+] uploader fix

--- src/main/java/ru/project/viviv/controller/ImageUploader.java
+++ src/main/java/ru/project/viviv/controller/ImageUploader.java
@@ -1,47 +1,41 @@
 package ru.project.viviv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.Principal;
 
 @Controller
-@RequestMapping("/uploader")
 public class ImageUploader {
-    private static String UPLOADED_FOLDER = "./images/";
 
-    @GetMapping("")
-    public String index() {
-        return "upload";
-    }
+    @Autowired
+    private HttpServletRequest request;
 
-    @PostMapping("/upload")
-    public String singleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
-        if (file.isEmpty()) {
-            model.addAttribute("message", "Please select a file to upload");
-            return "redirect:/uploader/uploadStatus";
+    private static String UPLOADED_FOLDER = "/images";
+
+    @PostMapping(value = "/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, Principal principal) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                new File("images/" + principal.getName()).mkdirs();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(UPLOADED_FOLDER + principal.getName() + "/" + file.getOriginalFilename())));
+                stream.write(bytes);
+                stream.close();
+                return "index";
+            } catch (Exception e) {
+                return "Вам не удалось загрузить => " + e.getMessage();
+            }
+        } else {
+            return "Вам не удалось загрузить потому что файл пустой.";
         }
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
-            model.addAttribute("message", "You successfully uploaded '" + file.getOriginalFilename() + "'");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "redirect:/uploader/uploadStatus";
-    }
-
-    @GetMapping("/uploadStatus")
-    public String uploadStatus() {
-        return "uploadStatus";
     }
 }
