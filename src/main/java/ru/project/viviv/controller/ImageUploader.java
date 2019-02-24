ebd38ed088aebe93413f900c63c2fecce96982f1
Aleksei Artemev <art.bmstu@gmail.com>
2019-02-25 00:54:08 +0300

    refactoring

--- src/main/java/ru/project/viviv/controller/ImageUploader.java
+++ src/main/java/ru/project/viviv/controller/ImageUploader.java
@@ -1,12 +1,12 @@
 package ru.project.viviv.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -15,27 +15,27 @@ import java.security.Principal;
 @Controller
 public class ImageUploader {
 
-    @Autowired
-    private HttpServletRequest request;
+    private static String UPLOADED_FOLDER = "images/";
+    private static final Logger log = LogManager.getLogger(ImageUploader.class);
 
-    private static String UPLOADED_FOLDER = "/images";
-
-    @PostMapping(value = "/upload")
+    @PostMapping(value = "upload")
     public String handleFileUpload(@RequestParam("file") MultipartFile file, Principal principal) {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
-                new File("images/" + principal.getName()).mkdirs();
+                File dir = new File(UPLOADED_FOLDER + principal.getName());
+                dir.mkdirs();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(UPLOADED_FOLDER + principal.getName() + "/" + file.getOriginalFilename())));
+                        new BufferedOutputStream(new FileOutputStream(new File(dir + "/" + file.getOriginalFilename())));
                 stream.write(bytes);
                 stream.close();
                 return "index";
             } catch (Exception e) {
-                return "Вам не удалось загрузить => " + e.getMessage();
+                log.info(principal.getName() + " не удалось загрузить аватар " + e.getMessage());
+                return "profile";
             }
         } else {
-            return "Вам не удалось загрузить потому что файл пустой.";
+            return "profile";
         }
     }
 }
